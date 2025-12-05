1cdc82ce726c77ad42456f6d1fa0e94cff79acb6
Letícia Lima <letslimadias183@gmail.com>
2025-12-05 09:15:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-const perguntas = [
-    { 
-        chave: ["essencial", "saúde", "criança"], 
-        resposta: "Alimentação equilibrada, vacinação em dia, higiene, acompanhamento pediátrico e ambiente seguro." 
-    },
-    { 
-        chave: ["frequência", "pediatra", "consulta"], 
-        resposta: "Nos primeiros anos, as consultas devem ser mensais ou trimestrais. Depois dos 2 anos, a cada 6 meses." 
-    },
-    { 
-        chave: ["vacinação", "importante", "vacina"], 
-        resposta: "As vacinas protegem contra doenças graves e fortalecem o sistema imunológico." 
-    },
-    { 
-        chave: ["sinal", "alerta", "atendimento", "médico"], 
-        resposta: "Febre alta persistente, dificuldade para respirar, vômitos intensos, manchas ou convulsões são sinais de alerta." 
-    },
-    { 
-        chave: ["imunidade", "fortalecer", "defesas"], 
-        resposta: "Boa alimentação, sono adequado, brincadeiras ao ar livre, vacinas em dia e evitar cigarro fortalecem a imunidade." 
-    }
-];
-
 function buscarResposta(mensagem) {
     const texto = mensagem.toLowerCase();
     let melhorResposta = null;
@@ -75,12 +52,6 @@ function enviarMensagem() {
     .catch(() => { 
         mostrarMensagem("Chatbot: Erro ao se conectar ao servidor.", "mensagem-bot");
     });
-
-    const resposta = buscarResposta(msg);
-
-    setTimeout(() => {
-        mostrarMensagem("Chatbot: " + resposta, "mensagem-bot");
-    }, 400);
 }
 
 let tamanhoFonte = 16;
